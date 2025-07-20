3216bae6bbb7dbf82a0ee0afeb1256e66868ed1e
Emily Jowers <emi.jowers@gmail.com>
2025-07-19 23:41:46 -0400

    Created generic layout for all pages. Added about page file

---
+++ src/pages/about.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-10 pt-20">
+      <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
+        About Me
+      </h2>
+      <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
+        I love working with startups and building products that people need.
+      </p>
+    </div>
+  );
+};
+
+export default About;
